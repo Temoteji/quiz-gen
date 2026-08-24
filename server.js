0135d7607eaf62f22be426a177dd538837f8b89c
Timothy James Sampelo <sampelotimothy2@gmail.com>
2026-08-25 07:13:40 +0800

    Update server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.NODE_ENV === 'production' 
-      ? "https://quiz-gen-topaz.vercel.app/auth/google/callback" 
+      ? `${process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : 'https://quiz-gen-topaz.vercel.app'}/auth/google/callback`
       : "http://localhost:3000/auth/google/callback",
     proxy: true
   },
